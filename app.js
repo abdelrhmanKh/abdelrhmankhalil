7f22e8480183b2ced6781255e44edcafb158a7b6
abdelrhmanKh <abdo.myownone@gmail.com>
2021-10-19 01:42:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 
 const btn = document.getElementById('sendMessage');
-
+var formText = {
+    from_name: document.getElementById('form_name').value,
+    phone_number: document.getElementById('phone_number').value,
+    price: document.getElementById('price').value,
+    message: document.getElementById('message').value,
+    email: document.getElementById('email').value,
+};
 document.getElementById('formProject')
     .addEventListener('submit', function (event) {
         event.preventDefault();
 
         btn.innerText = 'Sending...';
 
-        emailjs.sendForm("service_lftwgdg", "template_28hhmbm", this)
+        emailjs.sendForm("service_lftwgdg", "template_28hhmbm", formText)
             .then(() => {
                 btn.innerText = 'Send Message';
 
